c5aedfcb9c6bea178adca5ce048ce3a2af1f5ece
rokumura7 <rokumura.8807@gmail.com>
2020-10-18 00:48:56 +0900

    refactor

--- app/aws/OrchidS3.scala
+++ app/aws/OrchidS3.scala
@@ -11,33 +11,40 @@ import software.amazon.awssdk.services.s3.{S3Client, S3Configuration}
 import software.amazon.awssdk.services.s3.model._
 
 object OrchidS3 {
-  def get(objKey: String): ResponseInputStream[GetObjectResponse] =
-    getClient.getObject(OrchidS3RequestBuilder.getReq(objKey))
-  def put(objKey: String, file: File): PutObjectResponse =
-    getClient.putObject(OrchidS3RequestBuilder.putReq(objKey), file.toPath)
 
-  private def getClient: S3Client = {
-    val credential = AwsBasicCredentials.create(
+  private val credential =
+    AwsBasicCredentials.create(
       OrchidConf.aws.s3.accessKey,
       OrchidConf.aws.s3.secretKey
     )
-    val config = S3Configuration.builder()
+
+  private val s3Config =
+    S3Configuration.builder()
       .pathStyleAccessEnabled(true)
       .build()
+
+  private val client =
     S3Client.builder()
       .region(Region.AP_NORTHEAST_1)
       .credentialsProvider(StaticCredentialsProvider.create(credential))
       .endpointOverride(URI.create(OrchidConf.aws.s3.endPoint))
-      .serviceConfiguration(config)
+      .serviceConfiguration(s3Config)
       .build()
-  }
+
+  def get(objKey: String): ResponseInputStream[GetObjectResponse] =
+    client.getObject(OrchidS3RequestBuilder.getReq(objKey))
+
+  def put(objKey: String, file: File): PutObjectResponse =
+    client.putObject(OrchidS3RequestBuilder.putReq(objKey), file.toPath)
 
   private object OrchidS3RequestBuilder {
+
     def getReq(objKey: String): GetObjectRequest =
       GetObjectRequest.builder()
         .bucket(OrchidConf.aws.s3.bucket)
         .key(objKey)
         .build()
+
     def putReq(objKey: String): PutObjectRequest =
       PutObjectRequest.builder()
         .bucket(OrchidConf.aws.s3.bucket)
